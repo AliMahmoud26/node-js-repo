c64d1b33e0420bbcfbb11a3061c16757e3efb2a3
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-07-29 15:55:16 +0300

    commiting all the files to the github rep

---
+++ course instructions/instructions.js
@@ -0,0 +1,16 @@
+// Important Idioms and Definitions In The Course
+
+// ===========================================
+// ===========================================
+
+// 1- GLOBALS:
+// __dirname: path to the current directory
+// __filename: file name we are on
+// require: function to use modules (CommonJS)
+// module: info about current module (file)
+// process: info about env where the program is being executed
+
+// ===========================================
+// ===========================================
+
+// 2- Modules
---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-syntax')
+require('./mind-gernade')
+// console.log(names);
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
